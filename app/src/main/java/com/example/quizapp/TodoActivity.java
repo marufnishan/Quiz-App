1308c8e0144eb7a896347e95fa387934a443eb1d
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-08-11 12:43:40 +0600

    Firebase Data read Added

--- app/src/main/java/com/example/quizapp/TodoActivity.java
+++ app/src/main/java/com/example/quizapp/TodoActivity.java
@@ -1,5 +1,6 @@
 package com.example.quizapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,12 +8,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.quizapp.Adapter.ToDoAdapter;
+import com.example.quizapp.Model.ToDoModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TodoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FloatingActionButton mFab;
+    private FirebaseFirestore firestore;
+    private ToDoAdapter adapter;
+    private List<ToDoModel> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,7 @@ public class TodoActivity extends AppCompatActivity {
 
         recyclerView =findViewById(R.id.recycerlview);
         mFab = findViewById(R.id.floatingActionButton);
+        firestore = FirebaseFirestore.getInstance();
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(TodoActivity.this));
@@ -29,6 +45,32 @@ public class TodoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AddNewTask.newInstance().show(getSupportFragmentManager(),AddNewTask.TAG);
+            }
+        });
+
+        mList = new ArrayList<>();
+
+        adapter = new ToDoAdapter(TodoActivity.this,mList);
+
+        recyclerView.setAdapter(adapter);
+        showData();
+    }
+
+    private void showData(){
+        firestore.collection("task").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+
+                for (DocumentChange documentChange : value.getDocumentChanges()){
+                    if (documentChange.getType() == DocumentChange.Type.ADDED){
+                        String id = documentChange.getDocument().getId();
+                        ToDoModel toDoModel = documentChange.getDocument().toObject(ToDoModel.class).withId(id);
+
+                        mList.add(toDoModel);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                Collections.reverse(mList);
             }
         });
     }
